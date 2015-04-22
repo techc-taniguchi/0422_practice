73453122130beb6d28d3fce42c3d2e35cf04e658
TANIGUCHI Makoto <techctaniguchi@h2o-space.net>
2015-04-22 05:35:40 +0000

    add sample03

---
+++ sample03.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sample03</title>
+</head>
+<body>
+    <?php
+    print('現在は、' . dategdffsda('G:i:s') . 'です');
+    ?>
+</body>
+</html>
